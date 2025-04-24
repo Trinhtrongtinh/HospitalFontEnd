21ac503d5d00256e0130c829a7c80490f0d19bc9
Trinhtrongtinh <trinhtronngtinh1122@gmail.com>
2025-04-24 12:07:30 +0700

    chuyen-khoa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { YHocComponent } from './pages/news/y-hoc/y-hoc.component';
 import { SuKienComponent } from './pages/news/su-kien/su-kien.component';
 import { HoiThaoComponent } from './pages/news/hoi-thao/hoi-thao.component';
 import { BacSiComponent } from './pages/bac-si/bac-si.component';
+import { ChuyenKhoaComponent } from './pages/chuyen-khoa/chuyen-khoa.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     {path: 'tin-tuc/y-hoc', component:YHocComponent},
     {path:'tin-tuc/su-kien',component:SuKienComponent},
     {path:'tin-tuc/hoi-thao',component:HoiThaoComponent},
-    {path: 'bac-si', component:BacSiComponent}
+    {path: 'bac-si', component:BacSiComponent},
+    {path: 'chuyen-khoa', component:ChuyenKhoaComponent}
 ];
  
--- src/app/pages/chuyen-khoa/chuyen-khoa.component.ts
+++ src/app/pages/chuyen-khoa/chuyen-khoa.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chuyen-khoa',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './chuyen-khoa.component.html',
   styleUrl: './chuyen-khoa.component.scss'
 })
